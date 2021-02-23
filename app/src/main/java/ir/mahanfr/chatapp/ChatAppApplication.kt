8a8837d10dd2a8888eafe1d891158e2a2e1685c4
mahan.farzaneh2@gmail.com <mahan.farzaneh@gmail.com>
2021-02-23 23:18:02 +0330

    Fragments and Hilt Dagger Setup

---
+++ app/src/main/java/ir/mahanfr/chatapp/ChatAppApplication.kt
@@ -0,0 +1,9 @@
+package ir.mahanfr.chatapp
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ChatAppApplication : Application()
---
+++ app/src/main/java/ir/mahanfr/chatapp/ui/fragments/DashboardFragment.kt
@@ -0,0 +1,32 @@
+package ir.mahanfr.chatapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import ir.mahanfr.chatapp.R
+import ir.mahanfr.chatapp.ui.viewModels.DashboardViewModel
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
